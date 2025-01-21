33227fdd7c98d10e7dacab8560dc7e7a8a8a8851
Febi Salsabila <febisalsabillah05@gmail.com>
2025-01-21 23:07:55 +0700

    insert data ke database

--- myblog/app/Http/Controllers/PostController.php
+++ myblog/app/Http/Controllers/PostController.php
@@ -20,4 +20,44 @@ class PostController extends Controller
         //render view with posts
         return view('posts.index', compact('posts'));
     }
+    
+    /**
+     * create
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * store
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //validate form
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        //create post
+        Post::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
 }
