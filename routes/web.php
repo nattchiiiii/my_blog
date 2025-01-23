0fa145b0f3d33a5853c4d2344c1324ab1c64ab5e
Febi Salsabila <febisalsabillah05@gmail.com>
2025-01-23 08:59:55 +0700

    registrasi

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
